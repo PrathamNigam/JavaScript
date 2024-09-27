0465a12e3a96f7b1e7ebcc94a9779042d95e200b
Pratham Nigam <107574893+PrathamNigam@users.noreply.github.com>
2024-09-27 12:12:33 +0000

    datatypes-01

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "Pratham"
+let age = 18
+let isLoggedIn = false
+//number => 2 to power 53
+//bigInt
+//string=>""
+//boolean => True/False
+//null=> standalone value
+//undefined =>
+//symbol => unique
+//object
+
+console.log(typeof "Pratham")
+console.log(typeof undefined)
+console.log(typeof null)
