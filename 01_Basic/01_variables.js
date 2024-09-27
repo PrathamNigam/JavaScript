9290a4b6ad10876778e0cf96dfc90ab3594d6bb6
Pratham Nigam <107574893+PrathamNigam@users.noreply.github.com>
2024-09-27 12:07:54 +0000

    datatypes

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "pratham@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "p@gmail.com"
+accountPassword = "232123"
+accountCity = "Brngluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
