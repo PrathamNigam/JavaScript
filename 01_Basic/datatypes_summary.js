e6289e9679de729a19833beaf314ab2e755a8193
Pratham Nigam <107574893+PrathamNigam@users.noreply.github.com>
2024-09-28 14:17:05 +0000

    update

--- 01_Basic/datatypes_summary.js
+++ 01_Basic/datatypes_summary.js
@@ -5,4 +5,31 @@ let myObj = {
     name:"Pratham",
     age:22,
 }
-myfunct()
+//myfunct()
+
+
+
+//********************************Memory In JavaScript**************************
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+// In stack the data is copied it is the example for that
+
+let name = "pratham"
+let surname = name
+console.log(surname)
+surname = "nigam"
+console.log(surname)
+
+
+//In heap
+
+let userOne = {
+    email:"pratham@gmail",
+    upi:"ybl"
+}
+let userTwo = userOne
+userTwo.email = "s@"
+
+console.log(userOne.email)
+console.log(userTwo.email)
