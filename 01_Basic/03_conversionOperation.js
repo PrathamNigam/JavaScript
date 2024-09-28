e637ff3ddf2b33b8bb0c359be53194d7ca3ed29c
Pratham Nigam <107574893+PrathamNigam@users.noreply.github.com>
2024-09-28 10:48:30 +0000

    ConversionOperations

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,66 @@
+let score ="33"
+let marks = "45abc"
+let value1 = null;
+// console.log(typeof score)
+// console.log(typeof marks)
+// console.log(typeof value)
+
+let valueInNumber = Number(score);
+let nullInNumber = Number(value1);
+let marksInNumber = Number(marks);
+
+
+// "33"=>33
+// "33abc"=>NaN
+// true => 1; false =>0
+
+
+// console.log(typeof valueInNumber)
+// console.log(typeof marksInNumber)
+// console.log(typeof nullInNumber)
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//  1 => true; 0 =>false
+// ""=> false
+// "hitesh" => true
+
+
+// console.log(marksInNumber)
+// console.log(nullInNumber)
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ********************** Operations *****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = "Pratham"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2 +2)
+// console.log(1+2+"2")
+
+console.log(+true);
+console.log(+"")
+
+
+let num1,num2,num3
+//all set to 4
+num1 = num2 = num3 = 2 + 2
+
+
